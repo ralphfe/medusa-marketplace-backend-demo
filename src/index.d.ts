602fe2f8c6766492152c67ba7f7345507298974e
ralphfe <ralfs.fenuks@gmail.com>
2024-10-16 17:23:21 +0300

    Add product store relation

--- src/index.d.ts
+++ src/index.d.ts
@@ -11,3 +11,10 @@
       store?: Store;
     }
   }
+
+  export declare module "@medusajs/medusa/dist/models/product" {
+    declare interface Product {
+      store_id?: string;
+      store?: Store;
+    }
+  }
