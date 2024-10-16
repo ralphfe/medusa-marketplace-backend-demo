4a32b0dd033fb0b111ce890af41e29e96ac67791
ralphfe <ralfs.fenuks@gmail.com>
2024-10-16 13:34:31 +0300

    Adjust hardcoded PG fallback value

--- medusa-config.js
+++ medusa-config.js
@@ -29,7 +29,7 @@ const ADMIN_CORS =
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+  process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/medusa-starter-default";
 
 const REDIS_URL = process.env.REDIS_URL + "?family=0" || "redis://localhost:6379";
 
